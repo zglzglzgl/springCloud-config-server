7b64c3fdfe49e18e992a176a5856a6d6ef1b81e9
zgl <714490049@qq.com>
2019-02-22 10:23:37 +0800

    first commit

---
+++ springCloud/client/src/main/java/com/example/client/ClientApplication.java
@@ -0,0 +1,30 @@
+package com.example.client;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.xml.ws.RequestWrapper;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class ClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientApplication.class, args);
+    }
+
+
+    @Value("${server.port}")
+    String port;
+
+    @RequestMapping("/hello")
+    public String home(){
+        return "成功启动" + port;
+    }
+
+}
